79181c80a84953e97b8d643ab128e6a73f8da5a5
Sergey Todyshev <stodyshev@gmail.com>
2020-04-15 20:19:33 +0700

    cleanup

--- tests/chrono.spec.ts
+++ tests/chrono.spec.ts
@@ -1,12 +1,11 @@
 /**
  * @jest-environment node
  */
-import _ from "lodash";
 const wasm = require("../pkg/index.js");
 
 describe("chrono", () => {
   it("hour diff", () => {
-    let diff: any = {};
+    let diffs: any = {};
     for (let y = 0; y <= 2020; y++) {
       const d = new Date();
       d.setFullYear(y);
@@ -14,23 +13,17 @@ describe("chrono", () => {
       const expected = String(d.getHours());
       const actual = wasm.format_strftime("%H", d.getTime());
       if (actual !== expected) {
-        const d = parseInt(expected, 10) - parseInt(actual, 10);
-        const key = String(d);
-        if (!diff[key]) {
-          diff[key] = { ranges: [] };
-        }
-        const range = _.find(
-          diff[key].ranges,
-          (r) => y >= r[0] && y <= r[1] + 1
-        );
+        const key = String(parseInt(expected, 10) - parseInt(actual, 10));
+        const dif = diffs[key] || (diffs[key] = { ranges: [] });
+        const range = dif.ranges.find((r) => y >= r[0] && y <= r[1] + 1);
         if (range) {
           range[1] = y;
         } else {
-          diff[key].ranges.push([y, y]);
+          dif.ranges.push([y, y]);
         }
       }
     }
-    console.log(JSON.stringify(diff, null, "  "));
-    expect(Object.keys(diff).length).toBe(0);
+    console.log(JSON.stringify(diffs, null, "  "));
+    expect(Object.keys(diffs).length).toBe(0);
   });
 });
